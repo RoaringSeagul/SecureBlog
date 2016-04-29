250faac11da74bd1861294479bfcc8edf2b20cc0
RoaringSeagul <jokoro@cabanot.com>
2016-04-29 18:53:03 -0400

    Initial Commit

---
+++ secureblog/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Post, Message
+
+admin.site.register(Post)
---
+++ secureblog/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SecureblogConfig(AppConfig):
+    name = 'secureblog'
